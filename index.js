33be21fbf2567a77cde9f9fe1c8c78c76dca2dfd
Eliel <75944151+gilte@users.noreply.github.com>
2022-07-16 15:57:40 -0300

    update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var countDownDate = new Date("July 21, 2022 12:00:00").getTime();
- var x = setInterval(function() {  var now = new Date().getTime();
+ var y = setInterval(function() {  var now = new Date().getTime();
  var distance = countDownDate - now;
   var days = Math.floor(distance / (1000 * 60 * 60 * 24)); var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
   document.getElementById("dday").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-     if (distance < 0) { clearInterval(x); document.getElementById("dday").innerHTML = "FIM DO TESTE GRATUITO"; } }, 1000);
+     if (distance < 0) { clearInterval(y); document.getElementById("dday").innerHTML = "FIM DO TESTE GRATUITO"; } }, 1000);
