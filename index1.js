ce361a038a0ded002c13b5f5ffae02f8a6e47249
Eliel <75944151+gilte@users.noreply.github.com>
2022-07-18 21:30:10 -0300

    Rename index.js to index1.js

---
+++ index1.js
@@ -0,0 +1,8 @@
+var countDownDate = new Date("July 25, 2022 12:00:00").getTime();
+ var y = setInterval(function() {  var now = new Date().getTime();
+ var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24)); var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+ var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+ var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("dday").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+     if (distance < 0) { clearInterval(y); document.getElementById("dday").innerHTML = "FIM DO TESTE GRATUITO"; } }, 1000);
