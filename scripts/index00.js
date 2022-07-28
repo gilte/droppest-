2934caa52f4a2cbe3e51cafea7b9bd3d4e0738a5
Eliel <75944151+gilte@users.noreply.github.com>
2022-07-28 15:05:41 -0300

    update index00.js

--- scripts/index00.js
+++ scripts/index00.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("July 29, 2022 12:00:00").getTime();
+var countDownDate = new Date("august 04, 2022 12:00:00").getTime();
  var y = setInterval(function() {  var now = new Date().getTime();
  var distance = countDownDate - now;
   var days = Math.floor(distance / (1000 * 60 * 60 * 24)); var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
